ea86c81d3cbf8f29ce4980b22b4f4b17c9e38be8
Trevor Lee <trevorwslee@gmail.com>
2021-02-07 21:47:32 +0800

    added samples

---
+++ samples/arduino/ddautopin/ddautopin.ino
@@ -0,0 +1,84 @@
+#include <ssdumbdisplay.h>
+
+
+DumbDisplay dumbdisplay(new DDSoftwareSerialIO(new SoftwareSerial(2,3), DUMBDISPLAY_BAUD, true));
+
+
+LedGridDDLayer *rled;
+LedGridDDLayer *gled;
+LedGridDDLayer *bled;
+LedGridDDLayer *hmeter;
+LedGridDDLayer *vmeter;
+LcdDDLayer *lcd;
+
+int count = 20;
+
+void setup() {
+  // create R + G + B LED layers
+  rled = dumbdisplay.createLedGridLayer();
+  gled = dumbdisplay.createLedGridLayer();
+  bled = dumbdisplay.createLedGridLayer();
+
+  // create LED layers that will be used for "horizontal bar-meter"
+  hmeter = dumbdisplay.createLedGridLayer(2 * count, 1, 1, 5);
+  // create LED layers that will be used for "vertical bar-meter"
+  vmeter = dumbdisplay.createLedGridLayer(1, 2 * count, 5, 1);
+ 
+  // create a LCD layers with 2 rows of 16 characters
+  lcd = dumbdisplay.createLcdLayer(16, 2);
+
+  // configure to "auto pin" the different layers 
+  // -- end result of DD_AP_XXX(...) is the layout spec "H(V(0+1+2)+V(3+5)+4)"
+  // -- . H/V: layout direction
+  // -- . 0/1/2/3/4/5: layer id
+  dumbdisplay.configAutoPin(DD_AP_HORI_3(
+                              DD_AP_VERT_3(rled->getLayerId(), gled->getLayerId(), bled->getLayerId()),
+                              DD_AP_VERT_2(hmeter->getLayerId(), lcd->getLayerId()),
+                              vmeter->getLayerId()));
+
+  
+  // setup RGB leds color and turn them on
+  rled->onColor("red");
+  gled->onColor("green");
+  bled->onColor("blue");
+  rled->turnOn();
+  gled->turnOn();
+  bled->turnOn();
+
+  // set "bar meters" colors
+  hmeter->onColor("blue");
+  hmeter->offColor("yellow");
+  hmeter->backgroundColor("black");
+  vmeter->onColor("green");
+  vmeter->offColor("lightgray");
+  vmeter->backgroundColor("blue");
+
+  // set LCD colors and print out something
+  lcd->pixelColor("red");
+  lcd->bgPixelColor("lightgreen");
+  lcd->backgroundColor("black");
+  lcd->print("hello world");  
+  lcd->setCursor(0, 1);
+  lcd->print("how are you?");
+}
+
+void loop() {
+  delay(1000);
+  if (random(2) == 0) {
+    lcd->scrollDisplayLeft();
+    count--;
+  } else {  
+    lcd->scrollDisplayRight();
+    count++;
+  }
+  hmeter->horizontalBar(count);
+  vmeter->verticalBar(count);
+  if (random(2) == 0)
+    rled->toggle();
+  if (random(2) == 0)
+    gled->toggle();
+  if (random(2) == 0)
+    bled->toggle();
+}
+
+
---
+++ samples/arduino/ddpinturtle/ddpinturtle.ino
@@ -0,0 +1,73 @@
+#include <ssdumbdisplay.h>
+
+
+DumbDisplay dumbdisplay(new DDSoftwareSerialIO(new SoftwareSerial(2,3), DUMBDISPLAY_BAUD, true));
+
+
+TurtleDDLayer *turtle;
+int r = random(0, 255);
+int g = 128;
+int b = 0;
+
+void setup() {
+    // create a Turtle layer with size 240 x 190
+    turtle = dumbdisplay.createTurtleLayer(240, 190);
+    // setup Turtle layer
+    turtle->backgroundColor("azure");
+    turtle->fillColor("lemonchiffon");
+    turtle->penSize(1);
+    turtle->penFilled(true);
+    // initially draw something on the Turtle layer (will change some settings)
+    turtle->centeredPolygon(70, 6, true);
+    turtle->penFilled(false);
+    turtle->circle(80, true);
+
+    // create 4 LEDs -- left-top, right-top, right-bottom and left-bottom 
+    LedGridDDLayer* ltLed = dumbdisplay.createLedGridLayer();
+    LedGridDDLayer* rtLed = dumbdisplay.createLedGridLayer();
+    LedGridDDLayer* rbLed = dumbdisplay.createLedGridLayer();
+    LedGridDDLayer* lbLed = dumbdisplay.createLedGridLayer();
+
+    // set LEDs background color
+    ltLed->backgroundColor("green");
+    rtLed->backgroundColor("green");
+    rbLed->backgroundColor("green");
+    lbLed->backgroundColor("green");
+
+    // turn ON the LEDs
+    ltLed->turnOn();
+    rtLed->turnOn();
+    rbLed->turnOn();
+    lbLed->turnOn();
+
+    // config "pin frame" to be 290 units x 250 units
+    // 290: 25 + 240 + 25
+    // 240: 25 + 190 + 25
+    dumbdisplay.configPinFrame(290, 240);
+
+    // pin top-left LED @ (0, 0) with size (25, 25)
+    dumbdisplay.pinLayer(ltLed, 0, 0, 25, 25);
+    // pin top-right LED @ (265, 0) with size (25, 25)
+    dumbdisplay.pinLayer(rtLed, 265, 0, 25, 25);
+    // pin right-bottom LED @ (265, 215) with size (25, 25)
+    dumbdisplay.pinLayer(rbLed, 265, 215, 25, 25);
+    // pin left-bottom LED @ (0, 215) with size (25, 25)
+    dumbdisplay.pinLayer(lbLed, 0, 215, 25, 25);
+
+    // pin Turtle @ (25, 25) with size (240, 190)
+    dumbdisplay.pinLayer(turtle, 25, 25, 240, 190);
+}
+
+
+void loop() {
+    delay(1000);
+    turtle->penColor(DD_RGB_COLOR(r, g, b));
+    turtle->circle(27);
+    turtle->rectangle(90, 20);
+    turtle->rightTurn(10);
+    b = b + 20;
+    if (b > 255) {
+        b = 0;
+        r = random(0, 255);
+    }
+}
