59f1c711cbba8cc24c2a3975caee60b3eb518d8b
Trevor Lee <trevorwslee@gmail.com>
2021-09-10 01:56:35 +0800

    commit

--- test.ino
+++ test.ino
@@ -2,13 +2,16 @@
 // ESP32 built-in LED -- 2
 
 
-#include "dumbdisplay.h"
+#include "ssdumbdisplay.h"
 #include "ddtester.h"
 
 
-DumbDisplay dumbdisplay(new DDInputOutput(57600));
+//DumbDisplay dumbdisplay(new DDInputOutput(57600));
+DumbDisplay dumbdisplay(new DDSoftwareSerialIO(new SoftwareSerial(11, 10), 115200, true, 57600));
 
 void setup() {
+  // Serial.begin(57600);
+  // Serial.println("*****");
   dumbdisplay.debugSetup(13);  // setup to use pin 13
 
   if (true) {
