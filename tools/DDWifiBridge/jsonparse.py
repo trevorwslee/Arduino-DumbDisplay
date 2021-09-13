5b219c62fbd094a00d595470f6af96ad266216b7
Trevor Lee <trevorwslee@gmail.com>
2021-09-13 21:15:37 +0800

    working on simple JSON support

--- tools/DDWifiBridge/jsonparse.py
+++ tools/DDWifiBridge/jsonparse.py
@@ -15,7 +15,6 @@ class JsonStreamParserCore:
         self.field_value = None
         self.count = 0
         self.for_array = for_array
-        self.parsing_array = False
 
     def sinkJsonData(self, data):
         self.buffer = self.buffer + data
@@ -66,19 +65,19 @@ class JsonStreamParserCore:
             c = self.buffer[0]
             if c == '{' or c == '[':
                 self.buffer = self.buffer[1:]
-                self.state = '^>{'
-                self.parsing_array = c == '['
+                self.state = '^>' + c
             elif c == '"':
                 self.buffer = self.buffer[1:]
                 self.state = '^>"'
             else:
                 self.state = '^>'
-        if self.state == '^>{':
+        if self.state == '^>{' or self.state == '^>[':
+            parsing_array =  self.state == '^>['
             json_data = self.buffer
             if self.nested_parser == None:
-                self.nested_parser = JsonStreamParserCore(self.parsing_array)
+                self.nested_parser = JsonStreamParserCore(parsing_array)
                 self.nested_parser.onReceived = lambda field_name, field_value : self.onReceived(self.field_name + "." + field_name, field_value)
-                if self.parsing_array:
+                if parsing_array:
                     json_data = '[' + json_data
                 else:
                     json_data = '{' + json_data
@@ -99,7 +98,8 @@ class JsonStreamParserCore:
             self.count = self.count + 1
             self.state = '$'
         if self.state == '^>' or self.state == '$':
-            if self.for_array or self.parsing_array:
+            #if self.for_array or self.parsing_array:
+            if self.for_array:
                 close_token = ']'
             else:
                 close_token = '}'
@@ -209,25 +209,28 @@ class JsonStreamParserTester():
         self.expected2 = {'NESTED.str': 'str value', 'NESTED.int': '123', 'INT': '4321', 'NESTED2.str2': 'str value2', 'STR': 'STR VALUE'}
         self.json3 = '{ "str1": "str\\\\ing\\"1\\"", "int": 123, "str2" : "\\"string2\\"" }'
         self.expected3 = {'str1': 'str\\ing"1"', 'int': '123', 'str2': '"string2"'}
+        self.json4 = '{"int-arr":[123,456],"str-arr":["abc","def"],"single-level":"ABC", "nested":{"N1":11,"N2":222}, "nested-arr":[{"A":1,"B":2}, {"A":3,"B":4}]}'
+        self.expected4 = {'int-arr.0': '123', 'int-arr.1': '456', 'str-arr.0': 'abc', 'str-arr.1': 'def', 'single-level': 'ABC', 'nested.N1': '11', 'nested.N2': '222', 'nested-arr.0.A': '1', 'nested-arr.0.B': '2', 'nested-arr.1.A': '3', 'nested-arr.1.B': '4'}
 
     def testIt(self):
-        self._testTryArray()
+        #self._testTryArray()
         self._testDebugEscape()
         self._testIt(self.json1, self.expected1)
         self._testIt(self.json2, self.expected2)
         self._testIt(self.json3, self.expected3)
+        self._testIt(self.json4, self.expected4)
 
     def _testIt(self, json, expected_value):
         self._testSimple(json, expected_value)
         self._testPieceWise(json, expected_value)
 
-    def _testTryArray(self):
-        values = {}
-        parser = JsonStreamParser(lambda id, val: self._submit("S", values, id, val))
-        json = '{"int-arr":[123,456],"str-arr":["abc","def"],"single-level":"ABC", "nested":{"N1":11,"N2":222}, "nested-arr":[{"A":1,"B":2}, {"A":3,"B":4}]}'
-        #json = '{"nested-arr":[{"A":1,"B":2}, {"A":3,"B":4}]}'
-        parser.sinkJsonData(json)
-        print(values)
+    # def _testTryArray(self):
+    #     values = {}
+    #     parser = JsonStreamParser(lambda id, val: self._submit("S", values, id, val))
+    #     json = '{"int-arr":[123,456],"str-arr":["abc","def"],"single-level":"ABC", "nested":{"N1":11,"N2":222}, "nested-arr":[{"A":1,"B":2}, {"A":3,"B":4}]}'
+    #     #json = '{"nested-arr":[{"A":1,"B":2}, {"A":3,"B":4}]}'
+    #     parser.sinkJsonData(json)
+    #     print(values)
 
     def _testDebugEscape(self):
         expected_value = {'str1': 'str\\ing"abc"-def'}
@@ -239,8 +242,6 @@ class JsonStreamParserTester():
         if expected_value != values:
             print("XXX D XXX -- " + str(values))
             assert False
-
-
 
     def _testSimple(self, json, expected_value):
         values = self._runSimple(json)
