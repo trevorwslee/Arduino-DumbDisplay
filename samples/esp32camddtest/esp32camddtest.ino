4f4630e6cd15a78fd3bf4b54028fca9ffa099d00
Trevor Lee <31684028+trevorwslee@users.noreply.github.com>
2022-02-02 23:25:07 +0800

    commit

--- samples/esp32camddtest/esp32camddtest.ino
+++ samples/esp32camddtest/esp32camddtest.ino
@@ -2,23 +2,22 @@
 #include "esp_camera.h" 
 
 
-#include "esp_camera.h"         // https://github.com/espressif/esp32-camera
-
-
 
 #define BLUETOOTH
 
-
 #ifdef BLUETOOTH
+
 #include "esp32dumbdisplay.h"
-DumbDisplay dumbdisplay(new DDBluetoothSerialIO("ESP32Cam", true, 115200));
+DumbDisplay dumbdisplay(new DDBluetoothSerialIO("ESP32Cam"));
+
 #else
+
 #include "wifidumbdisplay.h"
 const char* ssid = <wifi SSID>;
 const char* password = <wifi password>;
 DumbDisplay dumbdisplay(new DDWiFiServerIO(ssid, password));
-#endif
 
+#endif
 
 
 
@@ -59,7 +58,6 @@ bool captureAndSaveImage(bool useFlash, bool cacheOnly);
 
 
 void setup() {
-
   flashLayer = createAndSetupButton(dumbdisplay);
   resolutionLayer = createAndSetupButton(dumbdisplay);
   captureModeLayer = createAndSetupButton(dumbdisplay, "red");
@@ -89,6 +87,7 @@ void loop() {
       flashLayer->writeCenteredLine("FLASH OFF");
     }  
   }
+
   if (streaming.record()) {
     if (streaming) {
       captureModeLayer->writeCenteredLine("STREAM");
@@ -96,6 +95,7 @@ void loop() {
       captureModeLayer->writeCenteredLine("STILL");
     }
   }
+
   if (imageSize.record()) {
     framesize_t frameSize;
     switch (imageSize) {
@@ -165,62 +165,34 @@ void loop() {
 
 
 
-
-
-
 const bool serialDebug = 1;                            // show debug info. on serial port (1=enabled, disable if using pins 1 and 3 as gpio)
 
 
-   #define PIXFORMAT PIXFORMAT_JPEG                     // image format, Options =  YUV422, GRAYSCALE, RGB565, JPEG, RGB888
-   //int cameraImageExposure = 0;                         // Camera exposure (0 - 1200)   If gain and exposure both set to zero then auto adjust is enabled
-   //int cameraImageGain = 0;                             // Image gain (0 - 30)
-   int cameraImageBrightness = 0;                       // Image brightness (-2 to +2)
+#define PIXFORMAT PIXFORMAT_JPEG                     // image format, Options =  YUV422, GRAYSCALE, RGB565, JPEG, RGB888
+int cameraImageBrightness = 0;                       // Image brightness (-2 to +2)
 
- //const int TimeBetweenStatus = 600;                     // speed of flashing system running ok status light (milliseconds)
+const int brightLED = 4;                             // onboard Illumination/flash LED pin (4)
+const int ledFreq = 5000;                            // PWM settings
+const int ledChannel = 15;                           // camera uses timer1
+const int ledRresolution = 8;                        // resolution (8 = from 0 to 255)
 
- //const int indicatorLED = 33;                           // onboard small LED pin (33)
-
- // Bright LED (Flash)
-   const int brightLED = 4;                             // onboard Illumination/flash LED pin (4)
-   //int brightLEDbrightness = 0;                         // initial brightness (0 - 255)
-   const int ledFreq = 5000;                            // PWM settings
-   const int ledChannel = 15;                           // camera uses timer1
-   const int ledRresolution = 8;                        // resolution (8 = from 0 to 255)
-
- //const int iopinA = 13;                                 // general io pin 13
- //const int iopinB = 12;                                 // general io pin 12 (must not be high at boot)
-
- //const int serialSpeed = 115200;                        // Serial data speed to use
-
- // NTP - Internet time
-  //  const char* ntpServer = "pool.ntp.org";
-  //  const char* TZ_INFO    = "GMT+0BST-1,M3.5.0/01:00:00,M10.5.0/02:00:00";  // enter your time zone (https://remotemonitoringsystems.ca/time-zone-abbreviations.php)
-  //  long unsigned lastNTPtime;
-  //  tm timeinfo;
-  //  time_t now;
-
-// camera settings (for the standard - OV2640 - CAMERA_MODEL_AI_THINKER)
-// see: https://randomnerdtutorials.com/esp32-cam-camera-pin-gpios/
-// set camera resolution etc. in 'initialiseCamera()' and 'cameraImageSettings()'
- #define CAMERA_MODEL_AI_THINKER
- #define PWDN_GPIO_NUM     32      // power to camera (on/off)
- #define RESET_GPIO_NUM    -1      // -1 = not used
- #define XCLK_GPIO_NUM      0
- #define SIOD_GPIO_NUM     26      // i2c sda
- #define SIOC_GPIO_NUM     27      // i2c scl
- #define Y9_GPIO_NUM       35
- #define Y8_GPIO_NUM       34
- #define Y7_GPIO_NUM       39
- #define Y6_GPIO_NUM       36
- #define Y5_GPIO_NUM       21
- #define Y4_GPIO_NUM       19
- #define Y3_GPIO_NUM       18
- #define Y2_GPIO_NUM        5
- #define VSYNC_GPIO_NUM    25      // vsync_pin
- #define HREF_GPIO_NUM     23      // href_pin
- #define PCLK_GPIO_NUM     22      // pixel_clock_pin
-
-
+#define CAMERA_MODEL_AI_THINKER
+#define PWDN_GPIO_NUM     32      // power to camera (on/off)
+#define RESET_GPIO_NUM    -1      // -1 = not used
+#define XCLK_GPIO_NUM      0
+#define SIOD_GPIO_NUM     26      // i2c sda
+#define SIOC_GPIO_NUM     27      // i2c scl
+#define Y9_GPIO_NUM       35
+#define Y8_GPIO_NUM       34
+#define Y7_GPIO_NUM       39
+#define Y6_GPIO_NUM       36
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       19
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM        5
+#define VSYNC_GPIO_NUM    25      // vsync_pin
+#define HREF_GPIO_NUM     23      // href_pin
+#define PCLK_GPIO_NUM     22      // pixel_clock_pin
 
 
 
@@ -229,7 +201,6 @@ bool cameraImageSettings() {
   if (serialDebug) Serial.println("Applying camera settings");
 
   sensor_t *s = esp_camera_sensor_get();
-  // something to try?:     if (s->id.PID == OV3660_PID)
   if (s == NULL) {
     if (serialDebug) Serial.println("Error: problem reading camera sensor settings");
     return 0;
@@ -249,62 +220,59 @@ bool cameraImageSettings() {
 void setupFlashPWM();
 
 bool initialiseCamera(framesize_t frameSize) {
-  if (true) {
-    esp_camera_deinit();     // disable camera
-    delay(50);
-    setupFlashPWM();    // configure PWM for the illumination LED
+  esp_camera_deinit();     // disable camera
+  delay(50);
+  setupFlashPWM();    // configure PWM for the illumination LED
+
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;               // XCLK 20MHz or 10MHz for OV2640 double FPS (Experimental)
+  config.pixel_format = PIXFORMAT;              // Options =  YUV422, GRAYSCALE, RGB565, JPEG, RGB888
+  config.frame_size = frameSize;                // Image sizes: 160x120 (QQVGA), 128x160 (QQVGA2), 176x144 (QCIF), 240x176 (HQVGA), 320x240 (QVGA),
+                                                //              400x296 (CIF), 640x480 (VGA, default), 800x600 (SVGA), 1024x768 (XGA), 1280x1024 (SXGA),
+                                                //              1600x1200 (UXGA)
+  config.jpeg_quality = 15;                     // 0-63 lower number means higher quality
+  config.fb_count = 1;                          // if more than one, i2s runs in continuous mode. Use only with JPEG
+
+  // check the esp32cam board has a psram chip installed (extra memory used for storing captured images)
+  //    Note: if not using "AI thinker esp32 cam" in the Arduino IDE, SPIFFS must be enabled
+  if (!psramFound()) {
+    if (serialDebug) Serial.println("Warning: No PSRam found so defaulting to image size 'CIF'");
+    config.frame_size = FRAMESIZE_CIF;
   }
 
-   camera_config_t config;
-   config.ledc_channel = LEDC_CHANNEL_0;
-   config.ledc_timer = LEDC_TIMER_0;
-   config.pin_d0 = Y2_GPIO_NUM;
-   config.pin_d1 = Y3_GPIO_NUM;
-   config.pin_d2 = Y4_GPIO_NUM;
-   config.pin_d3 = Y5_GPIO_NUM;
-   config.pin_d4 = Y6_GPIO_NUM;
-   config.pin_d5 = Y7_GPIO_NUM;
-   config.pin_d6 = Y8_GPIO_NUM;
-   config.pin_d7 = Y9_GPIO_NUM;
-   config.pin_xclk = XCLK_GPIO_NUM;
-   config.pin_pclk = PCLK_GPIO_NUM;
-   config.pin_vsync = VSYNC_GPIO_NUM;
-   config.pin_href = HREF_GPIO_NUM;
-   config.pin_sscb_sda = SIOD_GPIO_NUM;
-   config.pin_sscb_scl = SIOC_GPIO_NUM;
-   config.pin_pwdn = PWDN_GPIO_NUM;
-   config.pin_reset = RESET_GPIO_NUM;
-   config.xclk_freq_hz = 20000000;               // XCLK 20MHz or 10MHz for OV2640 double FPS (Experimental)
-   config.pixel_format = PIXFORMAT;              // Options =  YUV422, GRAYSCALE, RGB565, JPEG, RGB888
-   config.frame_size = frameSize;                // Image sizes: 160x120 (QQVGA), 128x160 (QQVGA2), 176x144 (QCIF), 240x176 (HQVGA), 320x240 (QVGA),
-                                                 //              400x296 (CIF), 640x480 (VGA, default), 800x600 (SVGA), 1024x768 (XGA), 1280x1024 (SXGA),
-                                                 //              1600x1200 (UXGA)
-   config.jpeg_quality = 15;                     // 0-63 lower number means higher quality
-   config.fb_count = 1;                          // if more than one, i2s runs in continuous mode. Use only with JPEG
+  esp_err_t camerr = esp_camera_init(&config);  // initialise the camera
+  if (camerr != ESP_OK) {
+    if (serialDebug) Serial.printf("ERROR: Camera init failed with error 0x%x", camerr);
+  }
 
-   // check the esp32cam board has a psram chip installed (extra memory used for storing captured images)
-   //    Note: if not using "AI thinker esp32 cam" in the Arduino IDE, SPIFFS must be enabled
-   if (!psramFound()) {
-     if (serialDebug) Serial.println("Warning: No PSRam found so defaulting to image size 'CIF'");
-     config.frame_size = FRAMESIZE_CIF;
-   }
+  cameraImageSettings();                        // apply custom camera settings
 
-   esp_err_t camerr = esp_camera_init(&config);  // initialise the camera
-   if (camerr != ESP_OK) {
-     if (serialDebug) Serial.printf("ERROR: Camera init failed with error 0x%x", camerr);
-   }
-
-   cameraImageSettings();                        // apply custom camera settings
-
-   return (camerr == ESP_OK);                    // return boolean result of camera initialisation
+  return (camerr == ESP_OK);                    // return boolean result of camera initialisation
 }
+
 
 // change illumination LED brightness
 void brightLed(byte ledBrightness){
    ledcWrite(ledChannel, ledBrightness);   // change LED brightness (0 - 255)
 }
-
-
 
 void setupFlashPWM() {
     ledcSetup(ledChannel, ledFreq, ledRresolution);
@@ -314,22 +282,15 @@ void setupFlashPWM() {
 }
 
 
-
 bool captureAndSaveImage(bool useFlash, bool cacheOnly) {
 
-  if (useFlash) brightLed(255);   // change LED brightness (0 - 255)
-  camera_fb_t *fb = esp_camera_fb_get();             // capture image frame from camera
-  if (useFlash) brightLed(0);   // change LED brightness back to previous state
+  if (useFlash) brightLed(255);            // change LED brightness (0 - 255)
+  camera_fb_t *fb = esp_camera_fb_get();   // capture image frame from camera
+  if (useFlash) brightLed(0);              // change LED brightness back to previous state
   if (fb == NULL) {
     if (serialDebug) Serial.println("Error: Camera capture failed");
      return false;
   }
-
-  // if (serialDebug) {
-  //    Serial.print("Captured image with ");
-  //    Serial.print(fb->len);
-  //    Serial.println(" bytes");
-  // }
 
   if (cacheOnly) {
     imageLayer->cacheImage(imageName, fb->buf, fb->len);
@@ -339,11 +300,8 @@ bool captureAndSaveImage(bool useFlash, bool cacheOnly) {
     dumbdisplay.writeComment("... save image");
   }
 
-
   esp_camera_fb_return(fb);        // return frame so memory can be released
 
   return true;
 }
-
-
 
