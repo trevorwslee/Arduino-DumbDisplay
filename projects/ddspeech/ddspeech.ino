8db522b05cfd00f5498202506402f9cb3b3ae7fa
Trevor Lee <31684028+trevorwslee@users.noreply.github.com>
2022-02-06 00:42:32 +0800

    commit

--- projects/ddspeech/ddspeech.ino
+++ projects/ddspeech/ddspeech.ino
@@ -1,20 +1,44 @@
+#define DUMBDISPLAY
+
+
+#ifdef DUMBDISPLAY
+#ifdef ESP32
+#include "esp32dumbdisplay.h"
+DumbDisplay dumbdisplay(new DDBluetoothSerialIO("ESP32", true, 115200));
+#else
+//#include "ssdumbdisplay.h"
+//DumbDisplay dumbdisplay(new DDSoftwareSerialIO(new SoftwareSerial(2, 3), 115200, true, 115200));
+#include "dumbdisplay.h"
+DumbDisplay dumbdisplay(new DDInputOutput(115200));
+#endif
+#endif
+
+
+// #ifdef ESP32
+// #define DISABLE_BROWNOUT
+// #endif
+// #ifdef DISABLE_BROWNOUT
+// // for disable brownout detection
+// #include "soc/soc.h"
+// #include "soc/rtc_cntl_reg.h"
+// #endif
+
+
+
 
 #ifdef ESP32
-
 #define synthesizer Serial2
-
 #else
-
 #include <SoftwareSerial.h>
 SoftwareSerial synthesizer(9, 8);  // pin 9 connect to TX of XFS5152CE board; 8 connect to RX of XFS5152CE board
-
 #endif
+
 
 
 #define TEST_VOICE
 
 
-int utf8ToUtf16(const char* utf8, uint8_t* utf16Buffer) {
+int UTF8ToUnicode(const char* utf8, uint8_t* utf16Buffer) {
     int idx = 0;
     int i = 0;
     while (true) {
@@ -53,17 +77,29 @@ int utf8ToUtf16(const char* utf8, uint8_t* utf16Buffer) {
 }
 
 
+#ifdef DUMBDISPLAY
+LcdDDLayer* lcd;
+#endif
+
 void setup() {
+// #ifdef ESP32
+// #ifdef DISABLE_BROWNOUT
+//   WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);     // Turn-off the 'brownout detector'
+// #endif
+// #endif
+
+#ifndef DUMBDISPLAY
     Serial.begin(115200);
+#endif
     synthesizer.begin(115200);
 
+#ifndef DUMBDISPLAY
     Serial.println("Hello! Let's get started!");
-
     if (true) {
         String text = "A科B大C讯D飞";
         //String text = "ABCD";
         uint8_t buffer[64];
-        int len = utf8ToUtf16(text.c_str(), buffer);
+        int len = UTF8ToUnicode(text.c_str(), buffer);
         for (int i = 0; i < len; i++) {
             uint8_t b = buffer[i];
             Serial.print(b, HEX);
@@ -71,6 +107,12 @@ void setup() {
         }
         Serial.println();
     }
+#endif
+
+#ifdef DUMBDISPLAY
+    lcd = dumbdisplay.createLcdLayer(32, 3);
+    lcd->enableFeedback("f:keys");
+#endif
 }
 
 
@@ -79,6 +121,14 @@ void setup() {
 long checkMillis = 0;
 bool isIdle = false;
 void loop() {
+#ifdef DUMBDISPLAY
+    DDFeedback* feedback = lcd->getFeedback();
+    if (feedback != NULL) {
+        if (feedback->text.length() > 0) {
+            lcd->writeLine(feedback->text, 1);
+        }
+    }
+#endif
 
 #ifdef TEST_VOICE
     if (isIdle) {
@@ -89,16 +139,18 @@ void loop() {
             String text = "[v1][h0]巴黎世家人氣老爹鞋低至6折!粉紅Triple S波鞋只需$5,880!";
             int text_len = text.length();
             uint8_t buffer[2 * text_len];
-            int len = utf8ToUtf16(text.c_str(), buffer);
+            int len = UTF8ToUnicode(text.c_str(), buffer);
             int out_len = 2 + len;
             synthesizer.write((byte)((out_len&0xFF00)>8));
             synthesizer.write((byte)(out_len&0x00FF));
             synthesizer.write((byte)0x01);
             synthesizer.write((byte)0x03);
+#ifndef DUMBDISPLAY
             Serial.print("{");
             Serial.print(text);
             Serial.print("}=");
             Serial.println(len);
+ #endif           
             synthesizer.write(buffer, len);
             //synthesizer.print(text);
             // for (int i = 0; i < len; i++) {
@@ -167,7 +219,11 @@ void loop() {
 
     if ((millis() - checkMillis) > 2000) {
         checkMillis = millis();
+#ifdef DUMBDISPLAY
+        dumbdisplay.writeComment("checking status ...");
+#else        
         Serial.println("checking status ...");
+#endif        
         synthesizer.write((byte)0xFD);
         synthesizer.write((byte)0x00);
         synthesizer.write((byte)0x01);
@@ -176,12 +232,20 @@ void loop() {
         if (synthesizer.available()) {
             int status = synthesizer.read();
             isIdle = status == 0x4F;
+#ifdef DUMBDISPLAY
+            if (isIdle) {
+                dumbdisplay.writeComment("... status=IDLE");
+            } else {
+                dumbdisplay.writeComment(String("... status=" + String(status)));
+            }       
+#else
             Serial.print("... status=");
             Serial.print(status, HEX);
             if (isIdle) {
                 Serial.print(" (IDLE)");
             }
             Serial.println();
+#endif            
         }
     }
 }
