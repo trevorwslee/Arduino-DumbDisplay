70c696d340f305470f6677ec90064aea211d3745
Trevor Lee <31684028+trevorwslee@users.noreply.github.com>
2022-06-27 19:48:36 +0800

    commit

--- dumbdisplay.h
+++ dumbdisplay.h
@@ -95,7 +95,7 @@ class DDLayer: public DDObject {
     /* - 7SegmentRowLayer; each 7-segment is composed of fixed 220 x 320 pixels */
     /* - LedGridLayer; a LED is considered as a pixel */  
     /* shape -- can be "flat", "hair", "round", "raised" or "sunken" */  
-    /* extraSize just added to size; however if shape is "round", it affect the "roundness" */
+    /* extraSize just added to size; however if shape is "round", it affects the "roundness" */
     void border(float size, const String& color, const String& shape = "flat", float extraSize = 0);
     void noBorder();
     /* size unit ... see border() */
