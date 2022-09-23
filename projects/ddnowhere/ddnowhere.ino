bdc82d30970356c2a95f40f77f123c1c17a83b0f
Trevor Lee <31684028+trevorwslee@users.noreply.github.com>
2022-09-23 12:06:17 +0800

    commit

--- projects/ddnowhere/ddnowhere.ino
+++ projects/ddnowhere/ddnowhere.ino
@@ -1,22 +1,13 @@
 
-//#define FOR_ESP32C3
 #define BLUETOOTH
 
-#ifdef FOR_ESP32C3
-    #define DD_4_ESP32
-    #include "esp32bledumbdisplay.h"
-    // - use ESP32 BLE with name "ESP32C3"
-    // - at the same time, enable Serial connection with 115200 baud 
-    DumbDisplay dumbdisplay(new DDBLESerialIO("ESP32C3", true));
+#include "ssdumbdisplay.h"
+#ifdef BLUETOOTH
+// assume HC-06 connected, to pin 2 and 3
+DumbDisplay dumbdisplay(new DDSoftwareSerialIO(new SoftwareSerial(2, 3), 115200, true));
 #else
-    #include "ssdumbdisplay.h"
-    #ifdef BLUETOOTH
-    // assume HC-06 connected, to pin 2 and 3
-    DumbDisplay dumbdisplay(new DDSoftwareSerialIO(new SoftwareSerial(2, 3), 115200, true));
-    #else
-    // otherwise, can use DumbDisplayWifiBridge -- https://www.youtube.com/watch?v=0UhRmXXBQi8
-    DumbDisplay dumbdisplay(new DDInputOutput(115200));
-    #endif
+// otherwise, can use DumbDisplayWifiBridge -- https://www.youtube.com/watch?v=0UhRmXXBQi8
+DumbDisplay dumbdisplay(new DDInputOutput(115200));
 #endif
 
 
@@ -25,33 +16,47 @@ TomTomMapDDLayer *tomtommap;
 BasicDDTunnel *datetimeTunnel;
 GpsServiceDDTunnel *gpsTunnel;
 
-void setup() {
-  // create a graphical LCD layer for showing the current date-time got  
-  graphical = dumbdisplay.createGraphicalLayer(230, 32);
-  graphical->backgroundColor("azure");
-  graphical->penColor("blue");
-  graphical->border(3, "darkgray");
-
-  // create a Tom Tom map layer
-  tomtommap = dumbdisplay.createTomTomMapLayer("", 300, 200);
-  tomtommap->border(5, "darkgray", "round");
-
-  // auto pin the two layers created above vertically
-  dumbdisplay.configAutoPin(DD_AP_VERT);
-
+void prepareTunnels() {
   // create a date-time service tunnel
   datetimeTunnel = dumbdisplay.createDateTimeServiceTunnel();
 
   // create a GPS service tunnel
   gpsTunnel = dumbdisplay.createGpsServiceTunnel();
   gpsTunnel->reconnectForLocation(2);  // reconnect to service for GPS location, continuously
+
 }
+
+void setup() {
+    // record the layout, basiclly for the ability of DD app reconnecting
+    dumbdisplay.recordLayerSetupCommands();
+  
+    // create a graphical LCD layer for showing the current date-time got  
+    graphical = dumbdisplay.createGraphicalLayer(230, 32);
+    graphical->backgroundColor("azure");
+    graphical->penColor("blue");
+    graphical->border(3, "darkgray");
+
+    // create a Tom Tom map layer
+    tomtommap = dumbdisplay.createTomTomMapLayer("", 300, 200);
+    tomtommap->border(5, "darkgray", "round");
+
+    // auto pin the two layers created above vertically
+    dumbdisplay.configAutoPin(DD_AP_VERT);
+
+    dumbdisplay.playbackLayerSetupCommands("rc-tomtommap");
+}
+
 
 
 String datetime;
 DDLocation location;
 
+DDConnectVersionTracker cvTracker(-1);
 void loop() {
+    if (cvTracker.checkChanged(dumbdisplay)) {
+        // there has been a reconnection ==> prepare tunnels
+        prepareTunnels();
+    }
     if (gpsTunnel->readLocation(location)) {
         // got GPS location feedback ==> sync TomTom map view
         tomtommap->goTo(location.latitude, location.longitude);
