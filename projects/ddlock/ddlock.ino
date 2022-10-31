28f088e56bd9d0d3bde118d16c5fa0174ffdf57f
Trevor Lee <31684028+trevorwslee@users.noreply.github.com>
2022-10-31 20:02:57 +0800

    c

--- projects/ddlock/ddlock.ino
+++ projects/ddlock/ddlock.ino
@@ -20,6 +20,9 @@ SimpleToolDDTunnel *tunnel_unlocked;
 SimpleToolDDTunnel *tunnel_locked;
 
 void setup() {
+  dumbdisplay.recordLayerSetupCommands();
+
+
   // create a LCD layer for display some info about the picture shown
   lcd = dumbdisplay.createLcdLayer(16, 1);
   lcd->bgPixelColor("darkgray");
@@ -32,16 +35,37 @@ void setup() {
   graphical->margin(2);
   graphical->border(2, "darkgreen");
 
+  graphical->enableFeedback();
+
   // auto "pin" the two layers vertically, one above the other
   dumbdisplay.configAutoPin(DD_AP_VERT);
+
+  dumbdisplay.playbackLayerSetupCommands("rc-lock");
+
 
   // create tunnels for downloading web images ... and save to your phone
   tunnel_unlocked = dumbdisplay.createImageDownloadTunnel("https://raw.githubusercontent.com/trevorwslee/Arduino-DumbDisplay/master/screenshots/lock-unlocked.png", "lock-unlocked.png");
   tunnel_locked = dumbdisplay.createImageDownloadTunnel("https://raw.githubusercontent.com/trevorwslee/Arduino-DumbDisplay/master/screenshots/lock-locked.png", "lock-locked.png");
 }
 
+long nextMillis = 0;
 bool locked = false;
+bool blinking = true;
+
 void loop() {
+  const DDFeedback* feedback = graphical->getFeedback();
+  if (feedback != NULL) {
+    if (feedback->type == DOUBLECLICK) {
+      blinking = !blinking;
+    }
+  }
+  long nowMillis = millis();
+  long diffMillis =  nextMillis - nowMillis;
+  if (diffMillis > 0) {
+    return;
+  }
+  nextMillis = nowMillis + 1000;
+
   // get result whether web image downloaded .. 0: downloading; 1: downloaded ok; -1: failed to download 
   int result_unlocked = tunnel_unlocked->checkResult();
   int result_locked = tunnel_locked->checkResult();
@@ -57,7 +81,11 @@ void loop() {
   }
   if (result == 1) {
     graphical->drawImageFile(image_file_name);
-    lcd->writeCenteredLine(locked ? "Lock locked" : "Lock unlocked");
+    if (blinking) {
+      lcd->writeCenteredLine(locked ? "Lock locked" : "Lock unlocked");
+    } else {
+      lcd->writeCenteredLine("paused");
+    }
   } else if (result == 0) {
     // downloading
     graphical->clear();
@@ -72,6 +100,7 @@ void loop() {
     graphical->println("XXX failed to download XXX");
     lcd->writeCenteredLine("failed");
   }
-  locked = !locked;
-  delay(1000);
+  if (blinking) {
+    locked = !locked;
+  }
 }
