a88fcb917c349194655655404ebb96a9ade51702
Trevor Lee <31684028+trevorwslee@users.noreply.github.com>
2023-02-25 16:36:25 +0800

    commit

--- projects/ddradar/ddradar.ino
+++ projects/ddradar/ddradar.ino
@@ -1,4 +1,3 @@
-#include <Servo.h>
 
 
 #define SERVO_PIN    11
@@ -7,21 +6,56 @@
 #define US_ECHO_PIN  10
 
 
+
+
+
+const int Width = 400;
+const int Height = Width / 2;
+const int W_half = Height;
+const int H = Height;
+const float DistFactor = W_half / 50.0;
+
 const int MaxAngle = 120;
 const int AngleIncrement = 1;
 
+const int A_start = (180 - MaxAngle) / 2;
 
+
+bool CalcCoor(int ang, int dist, int& x, int& y) {
+  float dist_norm = DistFactor * dist;
+  if (dist_norm > W_half) {
+    return false;
+  }
+  int ang_norm_i = 180 - (A_start + ang);
+  float rad_i = ang_norm_i * 3.1416 / 180.0;
+  float y_i = dist_norm * sin(rad_i);
+  float x_i = dist_norm * cos(rad_i);
+  x = W_half - x_i;
+  y = H - y_i;
+  return true;
+}
+
+
+
+#include "dumbdisplay.h"
+DumbDisplay dumbdisplay(new DDInputOutput());
+
+
+
+#include <Servo.h>
 Servo servo;
+
+
+
+GraphicalDDLayer* graphicalLayer;
+
 
 
 int angle = 0;
 bool angleIncreasing = true;
 
 void setup() {
-  Serial.begin(115200); // Starts the serial communication
-
   servo.attach(SERVO_PIN);
-
   servo.write(angle);
 
 
@@ -29,7 +63,8 @@ void setup() {
   pinMode(US_ECHO_PIN, INPUT); // Sets the echoPin as an Input
 
 
-  delay(1000);  // wait a bit first 
+  graphicalLayer = dumbdisplay.createGraphicalLayer(Width, Height);
+
 
 
 }
@@ -49,10 +84,17 @@ void loop() {
   long duration = pulseIn(US_ECHO_PIN, HIGH);
   // . calculating the distance ... 0.034: sound travels 0.034 cm per second
   int distance = duration * 0.034 / 2;
-  // . prins the distance on the Serial Monitor
-  Serial.print("Distance: ");
-  Serial.println(distance);
-  
+  //distance = 18;
+  int x;
+  int y;
+  int r = 3;
+  if (CalcCoor(angle, distance, x, y)) {
+    if (true) {
+      dumbdisplay.writeComment(String("Ang:") + angle + " Dist:" + distance + " X:" + x + " Y:" + y);
+    }
+    graphicalLayer->fillCircle(x, y, r);
+  }
+
 
   if (angleIncreasing) {
     angle += AngleIncrement;
