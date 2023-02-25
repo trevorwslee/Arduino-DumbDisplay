9617d51d10330d054189c40a7e59c2415d0c4254
Trevor Lee <31684028+trevorwslee@users.noreply.github.com>
2023-02-25 17:59:11 +0800

    commit

--- projects/ddradar/ddradar.ino
+++ projects/ddradar/ddradar.ino
@@ -13,6 +13,7 @@ const int Width = 400;
 const int Height = Width / 2;
 const int W_half = Height;
 const int H = Height;
+const int MaxDist = W_half - 20;
 const float DistFactor = W_half / 50.0;
 
 const int MaxAngle = 120;
@@ -21,9 +22,10 @@ const int AngleIncrement = 1;
 const int A_start = (180 - MaxAngle) / 2;
 
 
+
 bool CalcCoor(int ang, int dist, int& x, int& y) {
   float dist_norm = DistFactor * dist;
-  if (dist_norm > W_half) {
+  if (dist_norm > MaxDist) {
     return false;
   }
   int ang_norm_i = 180 - (A_start + ang);
@@ -36,24 +38,56 @@ bool CalcCoor(int ang, int dist, int& x, int& y) {
 }
 
 
-
-#include "dumbdisplay.h"
-DumbDisplay dumbdisplay(new DDInputOutput());
-
-
-
 #include <Servo.h>
 Servo servo;
 
 
 
-GraphicalDDLayer* graphicalLayer;
+#include "dumbdisplay.h"
+DumbDisplay dumbdisplay(new DDInputOutput());
+
+
+template<int MAX_LAYER_COUNT>
+class FadingLayers {
+  public:
+    FadingLayers() {
+      this->layerCount = 0;
+      this->nextUseLayerIdx = 0;
+    }
+    void initAddLayer(DDLayer* layer) {
+      if (layerCount < MAX_LAYER_COUNT) {
+        layers[layerCount++] = layer;
+      }
+    }
+  public:  
+    DDLayer* useLayer() {
+      int layerIdx = nextUseLayerIdx;
+      nextUseLayerIdx = (nextUseLayerIdx + 1) % layerCount;
+      for (int i = 0; i < this->layerCount; i++) {
+        int lidx = (layerIdx + i) % layerCount;
+        if (i == 0) {
+          layers[lidx]->clear();
+        } else {
+          int opacity = i * (100.0 / layerCount);
+          layers[lidx]->opacity(opacity);
+        }
+      }
+      return layers[layerIdx];
+    }  
+  private:
+    DDLayer* layers[MAX_LAYER_COUNT];
+    int layerCount;
+    int nextUseLayerIdx;
+};
+
+
+
+const int GrahpicalLayerCount = 3;
+FadingLayers<GrahpicalLayerCount> graphicalLayers; 
 
 
 
 int angle = 0;
-bool angleIncreasing = true;
-
 void setup() {
   servo.attach(SERVO_PIN);
   servo.write(angle);
@@ -63,11 +97,18 @@ void setup() {
   pinMode(US_ECHO_PIN, INPUT); // Sets the echoPin as an Input
 
 
-  graphicalLayer = dumbdisplay.createGraphicalLayer(Width, Height);
-
+  for (int i = 0; i < GrahpicalLayerCount; i++) {
+    GraphicalDDLayer* graphicalLayer = dumbdisplay.createGraphicalLayer(Width, Height);
+    graphicalLayer->noBackgroundColor();
+    graphicalLayers.initAddLayer(graphicalLayer);
+  }
 
 
 }
+
+
+bool angleIncreasing = true;
+GraphicalDDLayer* graphicalLayer = NULL;
 
 void loop() {
   servo.write(angle);
@@ -84,13 +125,19 @@ void loop() {
   long duration = pulseIn(US_ECHO_PIN, HIGH);
   // . calculating the distance ... 0.034: sound travels 0.034 cm per second
   int distance = duration * 0.034 / 2;
-  //distance = 18;
+
+  //distance = 40;
   int x;
   int y;
   int r = 3;
+
+  if (graphicalLayer == NULL) {
+    graphicalLayer = graphicalLayers.useLayer();
+  }
+  //GraphicalDDLayer* graphicalLayer = graphicalLayers.useLayer();
   if (CalcCoor(angle, distance, x, y)) {
     if (true) {
-      dumbdisplay.writeComment(String("Ang:") + angle + " Dist:" + distance + " X:" + x + " Y:" + y);
+      dumbdisplay.writeComment(String("Ang:") + angle + " Dist:" + distance + " X:" + x + " Y:" + y + " LID:" + graphicalLayer->getLayerId());
     }
     graphicalLayer->fillCircle(x, y, r);
   }
@@ -104,10 +151,10 @@ void loop() {
   if (angle > MaxAngle) {
     angle = MaxAngle;
     angleIncreasing = false;
-  } else {
-    if (angle < 0) {
-      angle = 0;
-      angleIncreasing = true;
-    }
+    graphicalLayer = NULL;
+  } if (angle < 0) {
+    angle = 0;
+    angleIncreasing = true;
+    graphicalLayer = NULL;
   }
 }
