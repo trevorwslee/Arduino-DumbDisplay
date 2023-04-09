5a059b3cbcc7a926a06a7cb49b53e25f7b596e99
Trevor Lee <31684028+trevorwslee@users.noreply.github.com>
2023-04-09 08:12:30 +0800

    commit

--- projects/esp32tensorflowlite/esp32cam_person/esp32cam_person.ino
+++ projects/esp32tensorflowlite/esp32cam_person/esp32cam_person.ino
@@ -1,31 +1,15 @@
+// if want Bluetooth, uncomment the following line
+// #define BLUETOOTH "ESP32BT"
+#if defined(BLUETOOTH)
+  #include "esp32dumbdisplay.h"
+  DumbDisplay dumbdisplay(new DDBluetoothSerialIO(BLUETOOTH));
+#else
+  #include "wifidumbdisplay.h"
+  DumbDisplay dumbdisplay(new DDWiFiServerIO(WIFI_SSID, WIFI_PASSWORD));
+#endif
+
 
 #include "esp_camera.h" 
-#include "esp32dumbdisplay.h"
-
-
-#if defined(FOR_LILYGO_TSIMCAM)
-// only support wifi
-#elif defined(FOR_LILYGO_TCAMERA)
-#define BLUETOOTH "LILYGOCAM"
-#else
-#define BLUETOOTH "ESP32CAM"
-#endif
-
-
-#ifdef BLUETOOTH
-
-#include "esp32dumbdisplay.h"
-DumbDisplay dumbdisplay(new DDBluetoothSerialIO(BLUETOOTH));
-
-#else
-
-#include "wifidumbdisplay.h"
-const char* ssid = WIFI_SSID;
-const char* password = WIFI_PASSWORD;
-DumbDisplay dumbdisplay(new DDWiFiServerIO(ssid, password));
-
-#endif
-
 
 #include <TensorFlowLite_ESP32.h>
 #include "tensorflow/lite/micro/micro_mutable_op_resolver.h"
