c5c17ed753a4bc6b4044451d459d8bc92a77f3a2
Trevor Lee <31684028+trevorwslee@users.noreply.github.com>
2023-08-07 23:19:38 +0800

    commit

--- ddoledutil.h
+++ ddoledutil.h
@@ -6,39 +6,15 @@
 #ifndef _dd_oled_util_h
 #define _dd_oled_util_h
 
-class OledDDDebugInterface: public DDDebugInterface {
+
+class OledDDDebugInterface: public DrawTextDDDebugInterface {
   public:
     OledDDDebugInterface(Adafruit_SSD1306& display, int x = 0, int y = 0/*, uint8_t fontSize = 2, uint8_t font = 1, */, bool indicateSendCommand = false): display(display) {
       this->x = x;
       this->y = y;
-      //this->fontSize = fontSize;
-      //this->font = font;
       this->indicateSendCommand = indicateSendCommand;
     }
   public:
-    virtual void logConnectionState(DDDebugConnectionState connectionState) {
-      const char* state = NULL;
-      switch (connectionState) {
-        case DDDebugConnectionState::DEBUG_NOT_CONNECTED:
-          state = "NCed";
-          break;
-        case DDDebugConnectionState::DEBUG_CONNECTING:
-          state = "Cing ";
-          break;
-        case DDDebugConnectionState::DEBUG_CONNECTED:
-          state = "Ced  ";
-          break;
-        case DDDebugConnectionState::DEBUG_RECONNECTING:
-          state = "RCing";
-          break;
-        case DDDebugConnectionState::DEBUG_RECONNECTED:
-          state = "RCed ";
-          break;
-      }
-      if (state != NULL) {
-        showMsg(state, 12/*16*/);
-      }
-    }
     virtual void logSendCommand(int state) {
       if (indicateSendCommand) {
         display.fillRect(x, y, x + 8, y + 8, WHITE);
@@ -46,15 +22,15 @@ class OledDDDebugInterface: public DDDebugInterface {
           display.fillCircle(x + 4, y + 4, 4, BLACK);
         }
         display.display();
-        // display.fillRect(x + 2, y + 2, x + 12, y + 12, WHITE);
-        // if (state == 1) {
-        //   display.fillCircle(x + 7, y + 7, 5, BLACK);
-        // }
-        // display.display();
       }
     }
-    virtual void logError(const String& errMsg) {
-        showMsg("Err", 70/*80*/);
+  protected:  
+    virtual void drawText(const char* text, bool isError) {
+      if (isError) {
+        showMsg(text, 70);
+      } else {
+        showMsg(text, 12);
+      }
     }
   private:  
     void showMsg(const char* msg, int xOff) {
@@ -74,5 +50,75 @@ class OledDDDebugInterface: public DDDebugInterface {
     int y;
     bool indicateSendCommand;
 };
+
+
+// class OledDDDebugInterface: public DDDebugInterface {
+//   public:
+//     OledDDDebugInterface(Adafruit_SSD1306& display, int x = 0, int y = 0/*, uint8_t fontSize = 2, uint8_t font = 1, */, bool indicateSendCommand = false): display(display) {
+//       this->x = x;
+//       this->y = y;
+//       //this->fontSize = fontSize;
+//       //this->font = font;
+//       this->indicateSendCommand = indicateSendCommand;
+//     }
+//   public:
+//     virtual void logConnectionState(DDDebugConnectionState connectionState) {
+//       const char* state = NULL;
+//       switch (connectionState) {
+//         case DDDebugConnectionState::DEBUG_NOT_CONNECTED:
+//           state = "NCed";
+//           break;
+//         case DDDebugConnectionState::DEBUG_CONNECTING:
+//           state = "Cing ";
+//           break;
+//         case DDDebugConnectionState::DEBUG_CONNECTED:
+//           state = "Ced  ";
+//           break;
+//         case DDDebugConnectionState::DEBUG_RECONNECTING:
+//           state = "RCing";
+//           break;
+//         case DDDebugConnectionState::DEBUG_RECONNECTED:
+//           state = "RCed ";
+//           break;
+//       }
+//       if (state != NULL) {
+//         showMsg(state, 12/*16*/);
+//       }
+//     }
+//     virtual void logSendCommand(int state) {
+//       if (indicateSendCommand) {
+//         display.fillRect(x, y, x + 8, y + 8, WHITE);
+//         if (state == 1) {
+//           display.fillCircle(x + 4, y + 4, 4, BLACK);
+//         }
+//         display.display();
+//         // display.fillRect(x + 2, y + 2, x + 12, y + 12, WHITE);
+//         // if (state == 1) {
+//         //   display.fillCircle(x + 7, y + 7, 5, BLACK);
+//         // }
+//         // display.display();
+//       }
+//     }
+//     virtual void logError(const String& errMsg) {
+//         showMsg("Err", 70/*80*/);
+//     }
+//   private:  
+//     void showMsg(const char* msg, int xOff) {
+//       const int charWidth = 6;
+//       for (int i = 0;; i++) {
+//         char c = msg[i];
+//         if (c == 0) {
+//           break;
+//         }
+//         display.drawChar(x + xOff + charWidth * i, y, c, WHITE, BLACK, 1);
+//       }
+//       display.display();
+//     }
+//   private:
+//     Adafruit_SSD1306& display;  
+//     int x;
+//     int y;
+//     bool indicateSendCommand;
+// };
 
 #endif
--- ddtftutil.h
+++ ddtftutil.h
@@ -7,7 +7,9 @@
 #ifndef _dd_tft_util_h
 #define _dd_tft_util_h
 
-class TftDDDebugInterface: public DDDebugInterface {
+#include "_dd_misc.h"
+
+class TftDDDebugInterface: public DrawTextDDDebugInterface {
   public:
     TftDDDebugInterface(TFT_eSPI& tft, int x = 0, int y = 0, uint8_t fontSize = 2, uint8_t font = 1, bool indicateSendCommand = false): tft(tft) {
       this->x = x;
@@ -17,29 +19,6 @@ class TftDDDebugInterface: public DDDebugInterface {
       this->indicateSendCommand = indicateSendCommand;
     }
   public:
-    virtual void logConnectionState(DDDebugConnectionState connectionState) {
-      const char* state = NULL;
-      switch (connectionState) {
-        case DDDebugConnectionState::DEBUG_NOT_CONNECTED:
-          state = "NCed";
-          break;
-        case DDDebugConnectionState::DEBUG_CONNECTING:
-          state = "Cing ";
-          break;
-        case DDDebugConnectionState::DEBUG_CONNECTED:
-          state = "Ced  ";
-          break;
-        case DDDebugConnectionState::DEBUG_RECONNECTING:
-          state = "RCing";
-          break;
-        case DDDebugConnectionState::DEBUG_RECONNECTED:
-          state = "RCed ";
-          break;
-      }
-      if (state != NULL) {
-        showMsg(state, 16);
-      }
-    }
     virtual void logSendCommand(int state) {
       if (indicateSendCommand) {
         tft.fillRect(x + 2, y + 2, x + 12, y + 12, TFT_WHITE);
@@ -48,8 +27,13 @@ class TftDDDebugInterface: public DDDebugInterface {
         }
       }
     }
-    virtual void logError(const String& errMsg) {
-        showMsg("Err", 80);
+  protected:  
+    virtual void drawText(const char* text, bool isError) {
+      if (isError) {
+        showMsg(text, 80);
+      } else {
+        showMsg(text, 16);
+      }
     }
   private:  
     void showMsg(const char* msg, int xOff) {
@@ -70,5 +54,70 @@ class TftDDDebugInterface: public DDDebugInterface {
     uint8_t font;
     bool indicateSendCommand;
 };
+
+
+// class TftDDDebugInterface: public DDDebugInterface {
+//   public:
+//     TftDDDebugInterface(TFT_eSPI& tft, int x = 0, int y = 0, uint8_t fontSize = 2, uint8_t font = 1, bool indicateSendCommand = false): tft(tft) {
+//       this->x = x;
+//       this->y = y;
+//       this->fontSize = fontSize;
+//       this->font = font;
+//       this->indicateSendCommand = indicateSendCommand;
+//     }
+//   public:
+//     virtual void logConnectionState(DDDebugConnectionState connectionState) {
+//       const char* state = NULL;
+//       switch (connectionState) {
+//         case DDDebugConnectionState::DEBUG_NOT_CONNECTED:
+//           state = "NCed";
+//           break;
+//         case DDDebugConnectionState::DEBUG_CONNECTING:
+//           state = "Cing ";
+//           break;
+//         case DDDebugConnectionState::DEBUG_CONNECTED:
+//           state = "Ced  ";
+//           break;
+//         case DDDebugConnectionState::DEBUG_RECONNECTING:
+//           state = "RCing";
+//           break;
+//         case DDDebugConnectionState::DEBUG_RECONNECTED:
+//           state = "RCed ";
+//           break;
+//       }
+//       if (state != NULL) {
+//         showMsg(state, 16);
+//       }
+//     }
+//     virtual void logSendCommand(int state) {
+//       if (indicateSendCommand) {
+//         tft.fillRect(x + 2, y + 2, x + 12, y + 12, TFT_WHITE);
+//         if (state == 1) {
+//           tft.fillCircle(x + 7, y + 7, 5, TFT_RED);
+//         }
+//       }
+//     }
+//     virtual void logError(const String& errMsg) {
+//         showMsg("Err", 80);
+//     }
+//   protected:  
+//     void showMsg(const char* msg, int xOff) {
+//       uint32_t textcolor = tft.textcolor;
+//       uint32_t textbgcolor = tft.textbgcolor;
+//       uint8_t textsize = tft.textsize;
+//       tft.setTextColor(TFT_RED, TFT_WHITE);
+//       tft.setTextSize(fontSize);
+//       tft.drawString(msg, x + xOff, y, font);
+//       tft.setTextColor(textcolor, textbgcolor);
+//       tft.setTextSize(textsize);
+//     }
+//   private:
+//     TFT_eSPI& tft;  
+//     int x;
+//     int y;
+//     uint8_t fontSize;
+//     uint8_t font;
+//     bool indicateSendCommand;
+// };
 
 #endif
