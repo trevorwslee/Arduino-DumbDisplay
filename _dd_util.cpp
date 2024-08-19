d0f879fee3f6ba7e403bc72baa1184f71c1a120d
Trevor Lee <trevorwslee@gmail.com>
2024-08-19 23:19:00 +0800

    updated

--- _dd_util.cpp
+++ _dd_util.cpp
@@ -1,6 +1,6 @@
 
 #include "Arduino.h"
-
+#include "_dd_util.h"
 
 extern bool _DDDisableParamEncoding;
 
@@ -44,7 +44,7 @@ char* _DDEncodeInt(int32_t i, char* buffer, int bufferLen) {
   return encoded;
 }
 
-void ParseGeneralServiceGetDataTimeResponse(const String& response, int& year, int& month, int& day, int& hour, int& minute, int& second) {
+void DDParseGetDataTimeResponse(const String& response, DDDateTime& dateTime) {
     String res = response;
     int idx = res.indexOf('-');
     String yyyy = res.substring(0, idx);
@@ -61,24 +61,24 @@ void ParseGeneralServiceGetDataTimeResponse(const String& response, int& year, i
     idx = res.indexOf('-');
     String mm = res.substring(0, idx);
     String ss = res.substring(idx + 1);
-    year = yyyy.toInt();
-    month = MM.toInt();
-    day = dd.toInt();
-    hour = hh.toInt();
-    minute = mm.toInt();
-    second = ss.toInt();
+    dateTime.year = yyyy.toInt();
+    dateTime.month = MM.toInt();
+    dateTime.day = dd.toInt();
+    dateTime.hour = hh.toInt();
+    dateTime.minute = mm.toInt();
+    dateTime.second = ss.toInt();
 }
 
 
 #if defined(ESP32)
-void Esp32SetDateTime(int year, int month, int day, int hour, int minute, int second) {
+void Esp32SetDateTime(const DDDateTime& dateTime) {
   struct tm timeinfo;
-  timeinfo.tm_year = year - 1900;
-  timeinfo.tm_mon = month - 1;
-  timeinfo.tm_mday = day;
-  timeinfo.tm_hour = hour;
-  timeinfo.tm_min = minute;
-  timeinfo.tm_sec = second;
+  timeinfo.tm_year = dateTime.year - 1900;
+  timeinfo.tm_mon = dateTime.month - 1;
+  timeinfo.tm_mday = dateTime.day;
+  timeinfo.tm_hour = dateTime.hour;
+  timeinfo.tm_min = dateTime.minute;
+  timeinfo.tm_sec = dateTime.second;
   time_t epoch = mktime(&timeinfo);
   int ms = 0;
 
@@ -98,17 +98,17 @@ void Esp32SetDateTime(int year, int month, int day, int hour, int minute, int se
   tv.tv_usec = ms;
   settimeofday(&tv, NULL);  
 }
-bool Esp32GetDateTime(int& year, int& month, int& day, int& hour, int& minute, int& second) {
+bool Esp32GetDateTime(DDDateTime& dateTime) {
   struct tm timeinfo;
   if (!getLocalTime(&timeinfo, 0)) {
     return false;
   }
-  year = timeinfo.tm_year + 1900;
-  month = timeinfo.tm_mon + 1;
-  day = timeinfo.tm_mday;
-  hour = timeinfo.tm_hour;
-  minute = timeinfo.tm_min;
-  second = timeinfo.tm_sec;
+  dateTime.year = timeinfo.tm_year + 1900;
+  dateTime.month = timeinfo.tm_mon + 1;
+  dateTime.day = timeinfo.tm_mday;
+  dateTime.hour = timeinfo.tm_hour;
+  dateTime.minute = timeinfo.tm_min;
+  dateTime.second = timeinfo.tm_sec;
   return true;
 }
 #endif
--- _dd_util.h
+++ _dd_util.h
@@ -1,12 +1,23 @@
 #ifndef _dd_util_h
 #define _dd_util_h
 
-#define GENERAL_SERVICE_CONNECT_FOR_GET_DATE_TIME "now:yyyy-MM-dd-HH-mm-ss"
-void ParseGeneralServiceGetDataTimeResponse(const String& response, int& year, int& month, int& day, int& hour, int& minute, int& second);
+#define DD_CONNECT_FOR_GET_DATE_TIME "now:yyyy-MM-dd-HH-mm-ss"
+class DDDateTime {
+  public:
+    DDDateTime(int year =  0, int month =  0, int day =  0, int hour =  0, int minute =  0, int second =  0): year(year), month(month), day(day), hour(hour), minute(minute), second(second) {
+    }
+    int year;
+    int month;
+    int day;
+    int hour;
+    int minute;
+    int second;
+};
+void DDParseGetDataTimeResponse(const String& response, DDDateTime& dateTime);
 
 #if defined(ESP32)
-void Esp32SetDateTime(int year, int month, int day, int hour, int minute, int second);
-bool Esp32GetDateTime(int& year, int& month, int& day, int& hour, int& minute, int& second);
+void Esp32SetDateTime(const DDDateTime& dateTime);
+bool Esp32GetDateTime(DDDateTime& dateTime);
 #endif
 
 
